9f3e48726578f2ee7f98191e6e10725855ed1b27
JC Grennan <jgrennan94@gmail.com>
2022-06-17 15:46:36 +0100

    finished info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,7 @@
 // importing my profile image from the images folder
 
 import profile from "../images/profile.jpeg"
+import email from "../images/Email.png"
 
 /* composing and exporting the Info component 
 inside a function component with JSX */
@@ -12,7 +13,7 @@ export default function Info() {
             <h1 className="info--name">Joshua C. Grennan</h1>
             <h2 className="info--title">Junior Frontend Developer</h2>
             <button className="info--email">
-                <img src="" alt=""/>Email
+                <img src={email} alt=""/>Email
             </button>
         </div>
     )
